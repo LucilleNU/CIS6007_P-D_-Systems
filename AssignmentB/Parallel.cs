64d695a7b913feb527a2a03e853b3b676e2efe81
LucilleNU <nawalucille@gmail.com>
2022-12-14 00:14:45 +0200

    update

--- AssignmentB/Parallel.cs
+++ AssignmentB/Parallel.cs
@@ -1,119 +1,86 @@
 using System;
 using System.Threading;
 
-namespace CirclePaintingSimulator
+namespace Assisgnment
 {
     class Program
     {
+        // Number of workers
+        static int K = 20;
+
+        // Number of circles
+        static int N = 1000;
+
+        // Delay for painting a circle (in milliseconds)
+        static int M = 20;
+
         static void Main(string[] args)
         {
             // Start time
             DateTime startTime = DateTime.Now;
-            
-            // Set the number of workers and the speed at which they move
-            int k = 20;
-            int m = 10;
 
-            // Set the number of circles and the time it takes to paint a circle
-            int n = 1000;
-            int paintingDelay = 20;
-            
-            Console.WriteLine($"{k} workers are assigned");
-            Console.WriteLine("----------");
-            
-
-            // Generate the coordinates of the circle centers
-            Random random = new Random();
-            int[,] circleCenters = new int[n, 2];
-            for (int i = 0; i < n; i++)
+            // Create an array to store the list of completed circles for each worker
+            bool[][] completed = new bool[K][];
+            for (int i = 0; i < K; i++)
             {
-                circleCenters[i, 0] = random.Next(0, 100);
-                circleCenters[i, 1] = random.Next(0, 100);
+                completed[i] = new bool[N];
             }
 
-            // Create the workers and assign them their sections of the field
-            Worker[] workers = new Worker[k];
-            int sectionSize = n / k;
-            for (int i = 0; i < k; i++)
+            // Create an array of threads for the workers
+            Thread[] workers = new Thread[K];
+
+            // Create a worker for each thread
+            for (int i = 0; i < K; i++)
             {
-                int start = i * sectionSize;
-                int end = (i + 1) * sectionSize - 1;
-                workers[i] = new Worker(i, m, circleCenters, start, end, paintingDelay);
+                int workerId = i;
+                workers[i] = new Thread(() => Worker(workerId, completed[workerId]));
+                workers[i].Start();
             }
 
-            // Start the workers
-            foreach (Worker worker in workers)
+            // Wait for all workers to finish
+            for (int i = 0; i < K; i++)
             {
-                worker.Start();
+                workers[i].Join();
             }
 
-            // Wait for all the workers to finish
-            foreach (Worker worker in workers)
-            {
-                worker.Join();
-            }
-            
-            Console.WriteLine("----------");
-            
-            // End time
+             // End time
             DateTime endTime = DateTime.Now;
 
-            // Print a message indicating that all the circles have been painted
-            Console.WriteLine("All circles have been painted!");
-            
+            // All circles are painted
+            Console.WriteLine("All circles are painted");
+
             // Print time taken to paint all the circles
             TimeSpan elapsed = endTime - startTime;
-            Console.WriteLine($"Time taken to paint all the circles: {elapsed.TotalMilliseconds - 20} milliseconds");
-        }
-    }
-
-    class Worker
-   {
-        private readonly int id;
-        private readonly int speed;
-        private readonly int[,] circleCenters;
-        private readonly int startIndex;
-        private readonly int endIndex;
-        private readonly int paintingDelay;
-        private readonly Thread thread;
-
-        public Worker(int id, int speed, int[,] circleCenters, int startIndex, int endIndex, int paintingDelay)
-        {
-            this.id = id;
-            this.speed = speed;
-            this.circleCenters = circleCenters;
-            this.startIndex = startIndex;
-            this.endIndex = endIndex;
-            this.paintingDelay = paintingDelay;
-
-            thread = new Thread(new ThreadStart(this.Run));
+            Console.WriteLine($"Time taken to paint all the circles by {K} workers: {elapsed.TotalMilliseconds - 20} milliseconds");
+            
         }
 
-        public void Start()
+        // Function that simulates a worker painting the circles
+        static void Worker(int id, bool[] completed)
         {
-            thread.Start();
-        }
-
-        public void Join()
-        {
-            thread.Join();
-        }
-        private void Run()
-        {
-            Console.WriteLine($"Worker {id} started");
-
-            for (int i = startIndex; i <= endIndex; i++)
+            for (int i = 0; i < N; i++)
             {
-                // Paint the circle at the given coordinates
-                int x = circleCenters[i, 0];
-                int y = circleCenters[i, 1];
-                //Console.WriteLine($"Worker {id} painting circle at ({x}, {y}//)");
+                // Check if the current circle has already been painted
+                bool painted = false;
+                for (int j = 0; j < K; j++)
+                {
+                    if (completed[j])
+                    {
+                        painted = true;
+                        break;
+                    }
+                }
 
-                // Simulate the time it takes to paint the circle
-                Thread.Sleep(paintingDelay);
+                // If the circle has not been painted, paint it
+                if (!painted)
+                {
+                    // Simulate painting the circle
+                    Thread.Sleep(M);
+
+                    // Mark the circle as completed
+                    completed[id] = true;
+                }
             }
-
-            Console.WriteLine($"Worker {id} finished");
         }
     }
 }
