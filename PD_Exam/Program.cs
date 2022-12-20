f1f632da5991481c56ea4ec249cf50c7d3664c60
LucilleNU <nawalucille@gmail.com>
2022-12-20 11:15:28 +0200

    Exam Section B

---
+++ PD_Exam/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PD_Exam
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Setting constants
+            const int N = 100;
+            const int max = 10000;
+
+            // Create an unsorted array of numbers
+            int[] numbers = Enumerable.Range(0, max).ToArray();
+
+            // Shuffle the array to make it unsorted
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int j = random.Next(i, numbers.Length);
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+
+            // Find a number bigger than N using a parallel algorithm
+            int output = FindNumberBiggerThanN(numbers, N);
+
+            // Print or display the result
+            Console.WriteLine($"Found a number bigger than {N}: {output}");
+        }
+
+        public static int FindNumberBiggerThanN(int[] numbers, int N)
+        {
+            // Use Parallel.For to search for a number bigger than N in parallel
+            int output = 0;
+            Parallel.For(0, numbers.Length, (i) =>
+            {
+                // If a number is found, set the result and exit the loop
+                if (numbers[i] > N)
+                {
+                    output = numbers[i];
+                    return;
+                }
+            });
+            return output;
+        }
+    }
+}
+
